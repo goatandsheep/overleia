7d11e2f9eff6d510a65825b31e337a1309e6883c
Rabia Agha <79723723+agharabia@users.noreply.github.com>
2021-06-17 13:32:08 -0400

    fixed the tests! yay they work now

--- test/index.test.js
+++ test/index.test.js
@@ -5,49 +5,51 @@ const fs = require('fs').promises;
 const settings = require('./settings');
 
 describe('basic ffmpeg', () => {
+    // test 1 
     it('should process an MP4 file', () => {
-        return expect(async () => {
-            Pip({
-                inputs: [
-                  path.join(__dirname, '..', settings.INPUT_DIRECTORY, settings.PIP_INPUT_IMG_FILENAME),
-                  path.join(__dirname, '..', settings.INPUT_DIRECTORY, settings.BASE_INPUT_MP4_FILENAME),
-                  path.join(__dirname, '..', settings.INPUT_DIRECTORY, settings.PIP_INPUT_MP4_FILENAME)
-                ],
-                output: path.join(__dirname, '..', settings.INPUT_DIRECTORY, settings.PIP_OUTPUT_FILENAME),
-                template: {
-                  "height": 720,
-                  "id": "0ac5c561-15dd-41d6-8448-41b427f9c1d8",
-                  "name": "bottom-middle-box",
-                  "views": [
-                    {
-                      "height": 250,
-                      "x": 45,
-                      "y": 45
-                    },
-                    {
-                      "height": 250,
-                      "x": 766,
-                      "y": 45
-                    },
-                    {
-                      "height": 250,
-                      "x": 408,
-                      "y": 387
-                    }
-                  ]
+        const prom = Pip({
+            inputs: [
+              path.join(__dirname, '..', settings.INPUT_DIRECTORY, settings.PIP_INPUT_IMG_FILENAME),
+              path.join(__dirname, '..', settings.INPUT_DIRECTORY, settings.BASE_INPUT_MP4_FILENAME),
+              path.join(__dirname, '..', settings.INPUT_DIRECTORY, settings.PIP_INPUT_MP4_FILENAME)
+            ],
+            output: path.join(__dirname, '..', settings.INPUT_DIRECTORY, settings.PIP_OUTPUT_FILENAME),
+            template: {
+              "height": 720,
+              "id": "0ac5c561-15dd-41d6-8448-41b427f9c1d8",
+              "name": "bottom-middle-box",
+              "views": [
+                {
+                  "height": 250,
+                  "x": 45,
+                  "y": 45
+                },
+                {
+                  "height": 250,
+                  "x": 766,
+                  "y": 45
+                },
+                {
+                  "height": 250,
+                  "x": 408,
+                  "y": 387
                 }
-              })
-        }).resolves.toBe(true);
+              ]
+            }
+          })
+        return expect(prom).resolves.toBe(true);
     })
+
+    // test 2
     it('should process 4 videos', async () => {
-        const inputs = [
-            path.join(__dirname, '..', settings.INPUT_DIRECTORY, settings.BASE_INPUT_MP4_FILENAME),
-            path.join(__dirname, '..', settings.INPUT_DIRECTORY, settings.PIP_INPUT_MP4_FILENAME),
-            path.join(__dirname, '..', settings.INPUT_DIRECTORY, settings.BASE_INPUT_MP4_FILENAME),
-            path.join(__dirname, '..', settings.INPUT_DIRECTORY, settings.PIP_INPUT_MP4_FILENAME)
-        ]
-        const out = await Pip({
-            inputs,
+        
+        const myProm = Pip({
+            inputs: [
+                    path.join(__dirname, '..', settings.INPUT_DIRECTORY, settings.BASE_INPUT_MP4_FILENAME),
+                    path.join(__dirname, '..', settings.INPUT_DIRECTORY, settings.PIP_INPUT_MP4_FILENAME),
+                    path.join(__dirname, '..', settings.INPUT_DIRECTORY, settings.BASE_INPUT_MP4_FILENAME),
+                    path.join(__dirname, '..', settings.INPUT_DIRECTORY, settings.PIP_INPUT_MP4_FILENAME)
+            ],
             template: {
                 views: [
                     {
@@ -72,7 +74,9 @@ describe('basic ffmpeg', () => {
                     }
                 ],
                 height: 720
-            }
-        }).resolves.toBe(true);
+                }
+        })
+        return expect(myProm).resolves.toBe(true);
+        
     })
 })
